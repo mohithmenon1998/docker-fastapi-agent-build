0cdb0a9d602850ecfa24f2892029a86b6d127a87
mohithmenon1998 <mohithmenon1998@gmail.com>
2026-07-15 10:16:03 +0530

    Added loguru messages

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from docker-fastapi-agent-build!")
+from loguru import logger
+import sys
+from backend.api.db import get_db_url, init_db
 
+def main():    
+    # 1. Clear default console logger to avoid duplicate screen prints
+    logger.remove() 
+    logger.add(sys.stderr, level="INFO")
+    # 2. Add your file logger EXACTLY ONCE here
+    logger.add("project.log", format="{time} | {level} | {message}")
+
+    logger.info("Main application started!")
+    get_db_url()
+    init_db()
 
 if __name__ == "__main__":
     main()
